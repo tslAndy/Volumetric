4d25fa8bcedc779e3b81f5162baa41f387d31dc3
Andy <andy.tsl@pm.me>
2026-08-28 10:48:49 +0300

    Improved voxelization

--- Assets/Scripts/Voxelizer.cs
+++ Assets/Scripts/Voxelizer.cs
@@ -7,7 +7,6 @@ public class Voxelizer
 
     private Vector3[] verts;
     private int[] trigs;
-    private Bounds bounds;
 
     private const float EPS = 0.001f;
 
@@ -15,8 +14,6 @@ public class Voxelizer
     {
         this.boxResolution = boxResolution;
         this.boxSize = boxSize;
-
-        this.bounds = model.GetComponent<MeshRenderer>().bounds;
 
         Mesh mesh = model.GetComponent<MeshFilter>().sharedMesh;
         this.verts = mesh.vertices;
@@ -30,36 +27,13 @@ public class Voxelizer
     public int[] GetVoxels()
     {
         int[] voxels = new int[boxResolution * boxResolution * boxResolution / 32];
-        for (int y = 0; y < boxResolution; y++)
-        {
-            for (int x = 0; x < boxResolution; x++)
-            {
-                for (int z = 0; z < boxResolution; z++)
-                {
-                    Vector3Int cpos = new Vector3Int(x, y, z);
-                    if (!IsVoxelOccupied(cpos))
-                        continue;
+        Norm[] flags = new Norm[boxResolution * boxResolution * boxResolution];
 
-                    int index = GetMorton(cpos);
-                    int div = index >> 5;
-                    int rem = index & 31;
-                    voxels[div] |= 1 << rem;
-                }
-            }
-        }
-        return voxels;
-    }
+        float worldToBox = 1.0f / boxSize * boxResolution;
+        float boxToWorld = 1.0f / boxResolution * boxSize;
 
-    private bool IsVoxelOccupied(Vector3Int cpos)
-    {
-        Vector3 worldPos = (Vector3.one * 0.5f + (Vector3)cpos) / boxResolution * boxSize;
+        float rad = 1.73205f * 0.5f * boxToWorld;
 
-        if (!bounds.Contains(worldPos))
-            return false;
-
-        Vector3 dir = Vector3.up;
-
-        int count = 0;
         for (int trigId = 0; trigId < trigs.Length; trigId += 3)
         {
             int indA = trigs[trigId];
@@ -70,39 +44,115 @@ public class Voxelizer
             Vector3 b = verts[indB];
             Vector3 c = verts[indC];
 
-            Vector3 min = Vector3.Min(Vector3.Min(a, b), c);
-            Vector3 max = Vector3.Max(Vector3.Max(a, b), c);
+            Vector3 norm = Vector3.Normalize(Vector3.Cross(b - a, c - b));
+            Norm flag = Vector3.Dot(norm, Vector3.up) > 0.0f ? Norm.Up : Norm.Down;
 
-            if (
-                worldPos.x < min.x
-                || worldPos.x > max.x
-                || worldPos.z < min.z
-                || worldPos.z > max.z
-            )
-                continue;
+            Vector3 na = Vector3.Normalize(Vector3.Cross(b - a, norm));
+            Vector3 nb = Vector3.Normalize(Vector3.Cross(c - b, norm));
+            Vector3 nc = Vector3.Normalize(Vector3.Cross(a - c, norm));
 
-            Vector3 surfVec = Vector3.Cross(b - a, c - b);
+            Vector3Int cmin = Vector3Int.FloorToInt(worldToBox * Vector3.Min(Vector3.Min(a, b), c));
+            Vector3Int cmax = Vector3Int.CeilToInt(worldToBox * Vector3.Max(Vector3.Max(a, b), c));
 
-            float div = Vector3.Dot(dir, surfVec);
-            if (Mathf.Abs(div) < EPS)
-                continue;
+            cmin = Vector3Int.Max(cmin, Vector3Int.zero);
+            cmax = Vector3Int.Min(cmax, Vector3Int.one * boxResolution);
 
-            float t = Vector3.Dot(a - worldPos, surfVec) / div;
-            if (t <= EPS)
-                continue;
+            for (int y = cmin.y; y < cmax.y; y++)
+            {
+                for (int x = cmin.x; x < cmax.x; x++)
+                {
+                    for (int z = cmin.z; z < cmax.z; z++)
+                    {
+                        int index = GetMorton(new Vector3Int(x, y, z));
 
-            Vector3 onTrig = worldPos + dir * t;
-            float uvA = Vector3.Dot(Vector3.Cross(onTrig - b, a - onTrig), surfVec);
-            float uvB = Vector3.Dot(Vector3.Cross(onTrig - a, c - onTrig), surfVec);
-            float uvC = Vector3.Dot(Vector3.Cross(onTrig - c, b - onTrig), surfVec);
+                        Vector3 center = new Vector3(x + 0.5f, y + 0.5f, z + 0.5f) * boxToWorld;
+                        Vector3 onTrig = center - norm * Vector3.Dot(center - a, norm);
 
-            if (uvA < 0.0 || uvB < 0.0 || uvC < 0.0)
-                continue;
+                        // point too far from trig plane
+                        if ((center - onTrig).sqrMagnitude > rad * rad)
+                            continue;
 
-            count++;
+                        float sideA = Vector3.Dot(onTrig - a, na);
+                        float sideB = Vector3.Dot(onTrig - b, nb);
+                        float sideC = Vector3.Dot(onTrig - c, nc);
+                        // point inside of triangle
+                        if (sideA <= 0.0f && sideB <= 0.0f && sideC <= 0.0f)
+                        {
+                            voxels[index >> 5] |= 1 << (index & 31);
+                            flags[index] |= flag;
+                            continue;
+                        }
+
+                        // clamp edges
+
+                        Vector3 p1,
+                            p2,
+                            n;
+
+                        if (sideA >= 0.0f)
+                            (p1, p2, n) = (a, b, na);
+                        else if (sideB >= 0.0f)
+                            (p1, p2, n) = (b, c, nb);
+                        else if (sideC >= 0.0f)
+                            (p1, p2, n) = (c, a, nc);
+                        else
+                            continue;
+
+                        onTrig = onTrig - n * Vector3.Dot(onTrig - p1, n);
+                        if (Vector3.Dot(onTrig - p1, p2 - p1) < 0.0f)
+                            onTrig = p1;
+                        else if (Vector3.Dot(p2 - onTrig, p2 - p1) < 0.0f)
+                            onTrig = p2;
+
+                        if ((center - onTrig).sqrMagnitude <= rad * rad)
+                        {
+                            voxels[index >> 5] |= 1 << (index & 31);
+                            flags[index] |= flag;
+                        }
+                    }
+                }
+            }
         }
 
-        return count % 2 == 1;
+        for (int x = 0; x < boxResolution; x++)
+        {
+            for (int z = 0; z < boxResolution; z++)
+            {
+                for (int y = 0; y < boxResolution; y++)
+                {
+                    bool foundDown = false,
+                        foundUp = false;
+
+                    for (int ty = y; ty >= 0; ty--)
+                    {
+                        Norm flag = flags[GetMorton(new Vector3Int(x, ty, z))];
+                        if ((flag & Norm.Down) != 0)
+                        {
+                            foundDown = true;
+                            break;
+                        }
+                    }
+
+                    for (int ty = y; ty < boxResolution; ty++)
+                    {
+                        Norm flag = flags[GetMorton(new Vector3Int(x, ty, z))];
+                        if ((flag & Norm.Up) != 0)
+                        {
+                            foundUp = true;
+                            break;
+                        }
+                    }
+
+                    if (foundUp && foundDown)
+                    {
+                        int index = GetMorton(new Vector3Int(x, y, z));
+                        voxels[index >> 5] |= 1 << (index & 31);
+                    }
+                }
+            }
+        }
+
+        return voxels;
     }
 
     private static int GetMorton(Vector3Int pos)
@@ -122,5 +172,12 @@ public class Voxelizer
             | ((n & 128) << 14)
             | ((n & 256) << 16)
             | ((n & 512) << 20);
+    }
+
+    private enum Norm : byte
+    {
+        None = 0,
+        Up = 1,
+        Down = 2,
     }
 }
